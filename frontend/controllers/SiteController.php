b1e898b8bfafeea3abcf688f74893d7c825f7f67
andrushai <andrusha_the_first@mail.ru>
2016-03-22 20:37:59 +0300

    add welcome

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -72,7 +72,8 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $name = Yii::$app->request->get('name');
+        return $this->render('index', ['name' => $name]);
     }
 
     /**
